1dd042f5c1ee132b2e1ba7adf9afdd51f5544178
Shoeb Ahmed Tanjim <39959228+s-a-tanjim@users.noreply.github.com>
2020-03-10 20:07:17 +0600

    Update servoControl.py

--- Project Arms/Scripts/servoControl.py
+++ Project Arms/Scripts/servoControl.py
@@ -1,47 +1,70 @@
+# 4 Servo can be controlled using this script
+# This script will fetch degree value of servo motor from MySQL database
+# By updating the degree value in database we can control Servo motor
+# Database table will be like:
+#      servoData        //Table Name
+# | ServoNo | Degree |
+# |    1    |   90   |
+# |    2    |   80   |
+# |    3    |   36   |
+# |    4    |   87   |
+
 import mysql.connector
 from mysql.connector import Error
 import RPi.GPIO as GPIO
 from time import sleep
 
 #Servo pin define
-servoPin1=11
-servoPin2=12
-servoPin3=13
+servoPin1=11    # GPIO 17
+servoPin2=12    # GPIO 18
+servoPin3=13    # GPIO 27
+servoPin4=15    # GPIO 22
+
+# Delay of servo
+ServoDelay = 0.2
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(servoPin1,GPIO.OUT)
 GPIO.setup(servoPin2,GPIO.OUT)
 GPIO.setup(servoPin3,GPIO.OUT)
+GPIO.setup(servoPin4,GPIO.OUT)
 
 pwm1=GPIO.PWM(servoPin1,50)
 pwm2=GPIO.PWM(servoPin2,50)
 pwm3=GPIO.PWM(servoPin3,50)
+pwm4=GPIO.PWM(servoPin4,50)
 pwm1.start(0)
 pwm2.start(0)
 pwm3.start(0)
+pwm4.start(0)
 
 def SetAngle(angle,ServoNo):
     duty=angle/18+2
     if(ServoNo==1):
         GPIO.output(servoPin1,True)
         pwm1.ChangeDutyCycle(duty)
-        sleep(1)
+        sleep(ServoDelay)
         GPIO.output(servoPin1,False)
         pwm1.ChangeDutyCycle(0)
     elif(ServoNo==2):
         GPIO.output(servoPin2,True)
         pwm2.ChangeDutyCycle(duty)
-        sleep(1)
+        sleep(ServoDelay)
         GPIO.output(servoPin2,False)
         pwm2.ChangeDutyCycle(0)
-        
     elif(ServoNo==3):
         GPIO.output(servoPin3,True)
         pwm3.ChangeDutyCycle(duty)
-        sleep(1)
+        sleep(ServoDelay)
         GPIO.output(servoPin3,False)
         pwm3.ChangeDutyCycle(0)
-        
+    elif(ServoNo==4):
+        GPIO.output(servoPin4,True)
+        pwm4.ChangeDutyCycle(duty)
+        sleep(ServoDelay)
+        GPIO.output(servoPin4,False)
+        pwm4.ChangeDutyCycle(0)
+
 #Main Loop
 try:
     while True:
@@ -62,7 +85,7 @@ try:
                 print("Servo No= ",row[0],"   |   Degree= ",row[1])
                 SetAngle(int(row[1]),ServoNo)
                 ServoNo+=1
-            sleep(0.5)
+            sleep(0.02)
 
         except Error as e:
             print("Error reading data from MySQL table", e)
@@ -76,7 +99,7 @@ except KeyboardInterrupt:
     pwm1.stop()
     pwm2.stop()
     pwm3.stop()
+    pwm4.stop()
     GPIO.cleanup()
     print("Ctl C pressed - ending program")
-
-        
+    
